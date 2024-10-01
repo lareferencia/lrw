36ba8b8c5396dfa39e222f99eb450b32d9e6a90b
lmatas <lmatas@gmail.com>
2024-10-01 09:30:15 +0000

    Update

---
+++ dist/chunks/lrw-chunk-FXBZOHQF.js
@@ -0,0 +1,1 @@
+import{a}from"./lrw-chunk-AJNE4HBJ.js";import"./lrw-chunk-TK4WJZKK.js";export{a as default};
---
+++ dist/chunks/lrw-chunk-NKIZC2DJ.js
@@ -0,0 +1,1 @@
+import{e as t,g as o,j as n,q as r,s as i}from"./lrw-chunk-TK4WJZKK.js";o();var e=t(n()),f=()=>(0,e.jsx)(i,{display:"flex",justifyContent:"center",alignItems:"center",height:"100%",children:(0,e.jsx)(r,{as:"h1",children:"No se encontr\xF3 informaci\xF3n"})}),a=f;export{a as default};
